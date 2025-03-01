48cc10e1b78775bfcce8f86f82f69cd0d50b469b
LIFE4649\tejim <tejima.tuyoshi@icloud.com>
2025-03-02 06:35:19 +0900

    長押し挙動作成

---
+++ Charge/Assets/Script/Ammo.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField] private InputActionReference _hold;
+    [SerializeField] bool isHold = false;
+    [SerializeField] bool isRemove = false;
+    private float power;
+    Rigidbody rb;
+    private void Awake()
+    {
+        if (_hold == null) return;
+
+        _hold.action.performed += OnHold;
+
+        _hold.action.canceled += OffHold;
+
+        _hold.action.Enable();
+
+        power = 0;
+
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (isHold)
+        {
+            power += 0.01f;
+            Debug.Log(power);
+        }
+
+        else if(isRemove)
+        {
+            Debug.Log("”­ŽË!");
+            isRemove = false;
+            rb.AddForce(-power, 0, 0, ForceMode.Impulse);
+            power = 0;
+        }
+    }
+
+    private void OnHold(InputAction.CallbackContext context)
+    {
+        isHold = true;
+    }
+
+    private void OffHold(InputAction.CallbackContext context)
+    {
+        isHold = false;
+        isRemove = true;
+    }
+}
